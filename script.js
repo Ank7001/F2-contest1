608e71d13728dd5278b4436b34acf752d84a75e6
Ank7001 <122182419+Ank7001@users.noreply.github.com>
2023-02-11 20:35:39 +0530

    Add script.js

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 /** @format */
 
 let arr = [
-  { id: 1, name: "john", age: "18", marks: 80 },
-  { id: 2, name: "jack", age: "20", marks: 85 },
-  { id: 3, name: "karen", age: "19", marks: 35 },
-];
-
-function PrintStudentswithMap() {
-  //Write your code here , just console.log
-}
-
-function PrintStudentsbyForEach() {
-  //Write your code here , just console.log
-}
-
-function addData() {
-  //Write your code here, just console.log
-}
-
-function removeFailedStudent() {
-  //Write your code here, just console.log
-}
-
-function concatenateArray() {
-  //Write your code here, just console.log
-}
+    { id: 1, name: "john", age: "18", marks: 80 },
+    { id: 2, name: "jack", age: "20", marks: 85 },
+    { id: 3, name: "karen", age: "19", marks: 35 },
+  ];
+  
+  function PrintStudentswithMap() {
+    //Write your code here , just console.log
+    var marksarr =  arr.map(function(item){
+        if(item.marks >= 50){
+        return item.name 
+     }}); 
+    
+      console.log(marksarr);
+  }
+  
+  function PrintStudentsbyForEach() {
+    //Write your code here , just console.log
+  }
+  
+  function addData() {
+    //Write your code here, just console.log
+  }
+  
+  function removeFailedStudent() {
+    //Write your code here, just console.log
+  }
+  
+  function concatenateArray() {
+    //Write your code here, just console.log
+  }
